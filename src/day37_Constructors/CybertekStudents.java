fc6de5c67ca72b0e196d2ac370a31e45e80a2891
silverhillus <silverhillus@gmail.com>
2020-05-12 03:01:39 -0400

    Day 37

--- src/day37_Constructors/CybertekStudents.java
+++ src/day37_Constructors/CybertekStudents.java
@@ -2,13 +2,13 @@ package day37_Constructors;
 
 public class CybertekStudents {
 
-    static String schoolName;
+    static String schoolName = "Cybertek University";
     String studentName; // we must need to know
     int groupNumber; // we must need to know
     String batch; // we must need to know
 
     public CybertekStudents( String studentName, int groupNumber, String batch){
-        schoolName = "Cybertek University";
+       // schoolName = "Cybertek University";
         this.studentName = studentName;
         this.groupNumber = groupNumber;
         this.batch = batch;
@@ -19,6 +19,24 @@ public class CybertekStudents {
                 ", in group: "+groupNumber+", School name: "+schoolName;
     }
 
+}
 
+
+class CObjects{
+
+    public static void main(String[] args) {
+        CybertekStudents student1 = new CybertekStudents("Drar", 3,"Batch 18");
+                student1.schoolName = "MIT";
+
+        System.out.println(student1);
+
+        CybertekStudents student2 = new CybertekStudents("Sarah", 12, "Batch 18");
+
+        System.out.println(student2);
+
+    }
 
 }
+
+
+
