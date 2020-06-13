2eb0bbbb96bb45ae35a0e44dfb6e6b479a56f557
silverhillus <silverhillus@gmail.com>
2020-06-12 20:34:28 -0400

    Day 40

--- src/day40_Encapsulation/CredentialsObject.java
+++ src/day40_Encapsulation/CredentialsObject.java
@@ -5,8 +5,8 @@ public class CredentialsObject {
     public static void main(String[] args) {
         Credentials AnnaFacebook = new Credentials();
 
-        AnnaFacebook.setUserName("A.paulura");
-        AnnaFacebook.setPassWord("12345667");
+        //AnnaFacebook.setUserName("A.paulura");
+        //AnnaFacebook.setPassWord("12345667");
 
         System.out.println( AnnaFacebook.getUserName() );
         System.out.println( AnnaFacebook.getPassWord() );
